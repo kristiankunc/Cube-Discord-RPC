88d8a2e98f532dd3660811f27e8eac9db33d4dbf
KristN <kristian.kunc@gmail.com>
2021-05-31 20:46:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import os
 import json
 import psutil
+from datetime import datetime
 
 print("Cube RPC started")
 
@@ -11,6 +12,7 @@ launcher_data_fullpath = appdata + "\.minecraft\launcher_profiles.json"
 logs_fullpath = appdata + "\.minecraft\logs\latest.log"
 
 mc = "javaw.exe"
+
 
 start_time = None
 
@@ -30,10 +32,10 @@ def getUserName():
 
         return profile["displayName"]
 
-def getMinigame():
+def getMinigame(logs):
     found = False
     minigames_list = ["Welcome to ||CubeCraft||", "SkyWars is starting", "EggWars is starting", "MinerWare is starting", "Among Slimes is starting", "Tower Defence is starting", "Lucky Islands is starting"]
-    for line in reversed(list(open(logs_fullpath))):
+    for line in logs:
         if found == False:
             for item in minigames_list:
                 if item in line:
@@ -70,38 +72,71 @@ def getMinigame():
                     if found == True:
                         return minigame
 
+def checkCube(logs):
+    for line in logs:
+        if "cubecraft.net., 25565" in line:
+            return True
 
-def checkCube():
-    with open(logs_fullpath,'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            if "Connecting to play.cubecraft.net., 25565" in line:
-                return True
+        if "Connecting" in line:
+            return False
+    return False
 
-def updatePresence():
-    state = f"Minigame - {getMinigame()}"
-    details = f"Username  - {getUserName()}"
-    large_image = "cclogo_lq"
-    small_image = None
-    large_text = "cubecraft.net"
-    
-    RPC.update(state=state, details=details, large_image=large_image, small_image=small_image, large_text=large_text, start=checkTime(start_time), buttons=[{"label": "Get this presence", "url": "https://github.com/KristN1/Cube-Discord-RPC"}])
+def afkCheck(logs):
+    current = datetime.now().minute
+    for line in logs:
+        print(logs)
+        print(line)
+        minutes = int(line.split(" ")[0].split(":")[1])
+        if current < minutes:
+            current += 60
+
+        print(f"current - {current}")
+        print(f"minutes - {minutes}")
+        print(f"counted - {current - 5} should be smaller than {minutes}")
+
+        if current - 5 <= minutes:
+            print("true")
+            return True
+        else:
+            print("false")
+            return False
+            
+def updatePresence(logs):
+        state = f"Minigame - {getMinigame(logs)}"
+        details = f"Username  - {getUserName()}"
+        large_image = "cclogo_lq"
+        small_image = None
+        large_text = "cubecraft.net"
+        
+        RPC.update(state=state, details=details, large_image=large_image, small_image=small_image, large_text=large_text, start=checkTime(start_time), buttons=[{"label": "Get this presence", "url": "https://github.com/KristN1/Cube-Discord-RPC"}])
 
 client_id = '827982404350115890' 
 RPC = Presence(client_id) 
 RPC.connect()
 
 while True:
-    
-    if mc in (p.name() for p in psutil.process_iter()):
+    with open(logs_fullpath,'r') as file:
+        logs = reversed(list(file))
+        if mc in (p.name() for p in psutil.process_iter()):
+            print("mccheck")
+            if checkCube(logs) == True:
+                print("cubechekc")
+                if afkCheck(logs) == False:
+                    print("afkcheck")
+                    print("AFK")
+                    RPC.clear()
+                    start_time = None
+                else:
+                    updatePresence(logs)
+                    print("ingame")
 
-        if checkCube() == True:
-            updatePresence()
-        else:
-            RPC.clear()
+            else:
+                start_time = None
+                RPC.clear()
         
-    else:
-        start_time = None
-        RPC.clear()
+        else:
+            start_time = None
+            RPC.clear()
 
+    print("-------------")
     time.sleep(15)
